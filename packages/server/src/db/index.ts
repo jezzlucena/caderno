35daf107aa3ad8c17cf791f26590a90815aca99b
Jezz Lucena <jezzlucena@gmail.com>
2026-01-18 21:42:45 -0800

    Remove old implementation

--- packages/server/src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-import * as schema from './schema.js'
-
-const connectionString = process.env.DATABASE_URL || 'postgres://caderno:caderno@db:5432/caderno'
-
-const client = postgres(connectionString)
-export const db = drizzle(client, { schema })
